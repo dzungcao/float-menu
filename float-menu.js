7156468608208983c951a48d7e079c548a38de7a
Dzung Cao <dung.cao@k-3ds.com>
2014-09-25 09:40:35 +0700

    Bug fixing for no float-section class found in html

--- float-menu.js
+++ float-menu.js
@@ -1,23 +1,25 @@
 $(function(){
-	var menu = $('<ul></ul>');
+	if($('.float-section').length > 0){
+		var menu = $('<ul></ul>');
 
-	for (var i = 0; i <= $('.float-section').length - 1; i++) {
-		var li = '<li><div class="float-element" title="'+$($('.float-section')[i]).attr('data-caption')+'"></div></li>';
-		menu = menu.append(li);
-	};
-
-	$("body").append('<div class="float-menu"><ul>' + $(menu).html() + '</ul></div>');
-
-	$(window).scroll(function(){
-		var top = $(this).scrollTop();
-		var index = 0;
 		for (var i = 0; i <= $('.float-section').length - 1; i++) {
-			var t = $($('.float-section')[i]).offset().top;
-			if(t-top >= 0){
-				$('.float-menu .float-element').removeClass('active');
-				$($('.float-menu ul li')[i]).find('.float-element').addClass('active');
-				break;
-			}
+			var li = '<li><div class="float-element" title="'+$($('.float-section')[i]).attr('data-caption')+'"></div></li>';
+			menu = menu.append(li);
 		};
-	});
+
+		$("body").append('<div class="float-menu"><ul>' + $(menu).html() + '</ul></div>');
+
+		$(window).scroll(function(){
+			var top = $(this).scrollTop();
+			var index = 0;
+			for (var i = 0; i <= $('.float-section').length - 1; i++) {
+				var t = $($('.float-section')[i]).offset().top;
+				if(t-top >= 0){
+					$('.float-menu .float-element').removeClass('active');
+					$($('.float-menu ul li')[i]).find('.float-element').addClass('active');
+					break;
+				}
+			};
+		});
+	}
 })
